54890098acb22336d36226ba73b8576561e22e94
Aditya verma <vermaaditya72061@gmail.com>
2024-02-23 21:42:19 +0530

    Update: Audio Issue Fixed : Final Commit

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ let constraints = {
         width: { min: 640, ideal: 1920, max: 1920 },
         height: { min: 480, ideal: 1080, max: 1080 },
     },
-    audio: false
+    audio: true
 };
 
 let init = async () => {
